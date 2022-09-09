a76a026270930750d6c0f9fe0d74bd79fd6643df
Green_Lab <greencoder001@outlook.com>
2022-09-09 18:16:30 +0200

    Maybe 30ms...

--- src/Devices/Weiche.js
+++ src/Devices/Weiche.js
@@ -31,7 +31,7 @@ module.exports = class Weiche extends Device {
         const done = await global.weichenmanager.myturn()
 
         this.leftPin.on()
-        await sleep(20)
+        await sleep(30)
         this.leftPin.off()
         this.state = 'left'
 
@@ -42,7 +42,7 @@ module.exports = class Weiche extends Device {
         const done = await global.weichenmanager.myturn()
 
         this.rightPin.on()
-        await sleep(20)
+        await sleep(30)
         this.rightPin.off()
         this.state = 'right'
 
