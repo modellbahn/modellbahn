ef58237ad85c2b8f509fc4d5ca0ed5164ef0a409
Green_Lab <greencoder001@outlook.com>
2022-09-09 18:10:43 +0200

    10ms again?

--- src/Devices/Weiche.js
+++ src/Devices/Weiche.js
@@ -31,7 +31,7 @@ module.exports = class Weiche extends Device {
         const done = await global.weichenmanager.myturn()
 
         this.leftPin.on()
-        await sleep(50)
+        await sleep(10)
         this.leftPin.off()
         this.state = 'left'
 
@@ -42,7 +42,7 @@ module.exports = class Weiche extends Device {
         const done = await global.weichenmanager.myturn()
 
         this.rightPin.on()
-        await sleep(50)
+        await sleep(10)
         this.rightPin.off()
         this.state = 'right'
 
